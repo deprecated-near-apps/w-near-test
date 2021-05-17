76fe4d309bb0bf0539f995a838a649751001c518
Josh <thisisjoshford@gmail.com>
2021-05-17 16:43:44 -0700

    update config

--- src/config.js
+++ src/config.js
@@ -1,36 +1,47 @@
-const contractName = 'dev-1613737629894-4341656';
+const CONTRACT_NAME = "dev-1613737629894-4341656";
 
 module.exports = function getConfig(isServer = false) {
-	let config = {
-		networkId: 'default',
-		nodeUrl: 'https://rpc.testnet.near.org',
-		walletUrl: 'https://wallet.testnet.near.org',
-		helperUrl: 'https://helper.testnet.near.org',
-		contractName,
-	};
-    
-	if (process.env.REACT_APP_ENV !== undefined) {
-		config = {
-			...config,
-			GAS: '200000000000000',
-			DEFAULT_NEW_ACCOUNT_AMOUNT: '5',
-			contractMethods: {
-				changeMethods: ['new', 'storage_deposit', 'near_deposit', 'near_withdraw', 'ft_transfer', 'ft_transfer_call'],
-				viewMethods: ['storage_minimum_balance', 'storage_balance_of', 'ft_balance_of'],
-			},
-		};
-	}
-    
-	if (process.env.REACT_APP_ENV === 'prod') {
-		config = {
-			...config,
-			networkId: 'mainnet',
-			nodeUrl: 'https://rpc.mainnet.near.org',
-			walletUrl: 'https://wallet.near.org',
-			helperUrl: 'https://helper.mainnet.near.org',
-			contractName: 'near',
-		};
-	}
+  let config = {
+    networkId: "testnet",
+    nodeUrl: "https://rpc.testnet.near.org",
+    walletUrl: "https://wallet.testnet.near.org",
+    helperUrl: "https://helper.testnet.near.org",
+    CONTRACT_NAME,
+  };
 
-	return config;
+  if (process.env.REACT_APP_ENV !== undefined) {
+    config = {
+      ...config,
+      GAS: "200000000000000",
+      DEFAULT_NEW_ACCOUNT_AMOUNT: "5",
+      contractMethods: {
+        changeMethods: [
+          "new",
+          "storage_deposit",
+          "near_deposit",
+          "near_withdraw",
+          "ft_transfer",
+          "ft_transfer_call",
+        ],
+        viewMethods: [
+          "storage_minimum_balance",
+          "storage_balance_of",
+          "ft_balance_of",
+        ],
+      },
+    };
+  }
+
+  if (process.env.REACT_APP_ENV === "prod") {
+    config = {
+      ...config,
+      networkId: "mainnet",
+      nodeUrl: "https://rpc.mainnet.near.org",
+      walletUrl: "https://wallet.near.org",
+      helperUrl: "https://helper.mainnet.near.org",
+      CONTRACT_NAME: "near",
+    };
+  }
+
+  return config;
 };
